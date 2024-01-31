406d96767fae08a1d0e2acf0aef902ce4e60a04c
ChenYCL <aa34913@gmail.com>
2024-02-01 00:13:19 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,29 +1,22 @@
 #!/bin/bash
 
 # 下载并解压
-curl -o apphub-linux-amd64.tar.gz https://assets.coreservice.io/public/package/60/app-market-gaga-pro/1.0.4/app-market-gaga-pro-1_0_4.tar.gz
-tar -zxf apphub-linux-amd64.tar.gz
-rm -f apphub-linux-amd64.tar.gz
-cd ./apphub-linux-amd64
+curl -o apphub-linux-arm64.tar.gz https://assets.coreservice.io/public/package/61/app-market-gaga-pro/1.0.4/app-market-gaga-pro-1_0_4.tar.gz && tar -zxf apphub-linux-arm64.tar.gz && rm -f apphub-linux-arm64.tar.gz && cd ./apphub-linux-arm64
 
 # 移除现有服务并安装新服务
-sudo ./apphub service remove
-sudo ./apphub service install
+sudo ./apphub service remove && sudo ./apphub service install
 
 # 启动服务
 sudo ./apphub service start
+./apphub status
+sudo ./apps/gaganode/gaganode config set --token=dsplkvwusnrtrbnvbcbe52adac6ced70
+./apphub restart
 
 # 检查应用状态
-./apphub status | grep 'gaganode status is RUNNING'
+./apphub status | grep 'status:[RUNNING]'
 if [ $? -ne 0 ]; then
     echo "Gaganode is not running."
     exit 1
+else
+    echo "Gaganode is running successfully."
 fi
-
-# 设置token
-sudo ./apps/gaganode/gaganode config set --token=dsplkvwusnrtrbnvbcbe52adac6ced70
-
-# 重启应用
-sudo ./apphub restart
-
-echo "Gaganode has been installed and started successfully."
